fc6cf08d9c2bd13296783edd0a10eeac586b0182
jmalisz <jakub.maliszewski.p@gmail.com>
2023-12-12 00:14:14 +0100

    Add cleanup for registerListenerAgentCalculateMove

--- src/events/listeners/registerListenerAgentMove.ts
+++ src/events/listeners/registerListenerAgentMove.ts
@@ -6,19 +6,17 @@ const SUBJECT = "agent.stockfish.calculateMove";
 
 type AgentCalculateMoveCallback = (payload: GameData) => Promise<void> | void;
 
-export const registerListenerAgentCalculateMove = (
-  natsClient: NatsConnection,
-  jsonCodec: Codec<Record<string, unknown>>,
-) => {
-  const subscription = natsClient.subscribe(SUBJECT);
+export const registerListenerAgentCalculateMove =
+  (natsClient: NatsConnection, jsonCodec: Codec<Record<string, unknown>>) =>
+  (callback: AgentCalculateMoveCallback) => {
+    const subscription = natsClient.subscribe(SUBJECT);
 
-  return (callback: AgentCalculateMoveCallback) => {
-    // eslint-disable-next-line no-void
     void (async () => {
       for await (const message of subscription) {
         const agentMovePayload = gameDataSchema.parse(jsonCodec.decode(message.data));
         await callback(agentMovePayload);
       }
     })();
+
+    return () => subscription.drain();
   };
-};
